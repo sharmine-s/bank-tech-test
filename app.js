47d21a066d087000d3f6147945ee66aa8ff2a3d5
sharmine-s <sharmine.ss@gmail.com>
2023-06-19 12:04:06 +0100

    re-initialised repo and setup rspec and rubocop to use ruby instead

--- app.js
+++
@@ -1,6 +0,0 @@
-const app = () => {
-  console.log("hello")
-}
-
-
-module.exports = app;
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'app'
+
+RSpec.describe('Bank tech test') do
+  it('Runs rspec correctly') do
+    expect(test).to eq('hello')
+  end
+end
